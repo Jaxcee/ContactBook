b2bbf765ab3f995a618183af88067cca15a9a8ce
JAGADESH J <you@example.com>
2024-02-20 10:49:12 +0530

    Adduser

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbook;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
@@ -24,8 +25,47 @@ public class AddressBook {
     }
 
     // Getter methods
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 
     // Setter methods
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public void setAddress(String address) {
         this.address = address;
     }
@@ -63,79 +103,111 @@ public class AddressBook {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<AddressBook> addressBookList = new ArrayList<>();
 
-        System.out.println("Enter First Name:");
-        String firstName = scanner.nextLine();
-        System.out.println("Enter Last Name:");
-        String lastName = scanner.nextLine();
+        char choice;
+        do {
+            System.out.println("Enter First Name:");
+            String firstName = scanner.nextLine();
+            System.out.println("Enter Last Name:");
+            String lastName = scanner.nextLine();
 
-        System.out.println("Enter Address: ");
-        String address = scanner.nextLine();
+            System.out.println("Enter Address: ");
+            String address = scanner.nextLine();
 
-        System.out.println("Enter City: ");
-        String city = scanner.nextLine();
+            System.out.println("Enter City: ");
+            String city = scanner.nextLine();
 
-        System.out.println("Enter State: ");
-        String state = scanner.nextLine();
+            System.out.println("Enter State: ");
+            String state = scanner.nextLine();
 
-        System.out.println("Enter ZIP: ");
-        String zip = scanner.nextLine();
+            System.out.println("Enter ZIP: ");
+            String zip = scanner.nextLine();
 
-        System.out.println("Enter Phone Number: ");
-        String phoneNumber = scanner.nextLine();
+            System.out.println("Enter Phone Number: ");
+            String phoneNumber = scanner.nextLine();
 
-        System.out.println("Enter Email: ");
-        String email = scanner.nextLine();
+            System.out.println("Enter Email: ");
+            String email = scanner.nextLine();
 
-        AddressBook newContact = new AddressBook(firstName, lastName, address, city, state, zip, phoneNumber, email);
-        System.out.println(newContact);
-        newContact.displayContact();
+            AddressBook newContact = new AddressBook(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            System.out.println(newContact);
+            newContact.displayContact();
+
+            addressBookList.add(newContact);
+
+            System.out.println("Do you want to add another contact? (y/n)");
+            choice = scanner.nextLine().charAt(0);
+        } while (Character.toLowerCase(choice) == 'y');
+
 
         System.out.println("Do you want to edit or delete any contact? (edit/delete/no)");
-        String choice = scanner.nextLine();
+        String editOrDeleteChoice = scanner.nextLine();
 
-        if (choice.equalsIgnoreCase("edit")) {
-            System.out.println("What do you want to edit? (address/city/state/zip/phoneNumber/email)");
-            String fieldToEdit = scanner.nextLine();
-            System.out.println("Enter new value:");
-            String newValue = scanner.nextLine();
+        if (editOrDeleteChoice.equalsIgnoreCase("edit")) {
+            // Edit contact
+            System.out.println("Enter the index of the contact you want to edit:");
+            int index = scanner.nextInt();
+            scanner.nextLine(); // Consume newline character
+            if (index >= 0 && index < addressBookList.size()) {
+                AddressBook contactToEdit = addressBookList.get(index);
+                System.out.println("What do you want to edit? (address/city/state/zip/phoneNumber/email)");
+                String fieldToEdit = scanner.nextLine();
+                System.out.println("Enter new value:");
+                String newValue = scanner.nextLine();
 
-            switch (fieldToEdit) {
-                case "address":
-                    newContact.setAddress(newValue);
-                    break;
-                case "city":
-                    newContact.setCity(newValue);
-                    break;
-                case "state":
-                    newContact.setState(newValue);
-                    break;
-                case "zip":
-                    newContact.setZip(newValue);
-                    break;
-                case "phoneNumber":
-                    newContact.setPhoneNumber(newValue);
-                    break;
-                case "email":
-                    newContact.setEmail(newValue);
-                    break;
-                default:
-                    System.out.println("Invalid field to edit!");
+                switch (fieldToEdit) {
+                    case "address":
+                        contactToEdit.setAddress(newValue);
+                        break;
+                    case "city":
+                        contactToEdit.setCity(newValue);
+                        break;
+                    case "state":
+                        contactToEdit.setState(newValue);
+                        break;
+                    case "zip":
+                        contactToEdit.setZip(newValue);
+                        break;
+                    case "phoneNumber":
+                        contactToEdit.setPhoneNumber(newValue);
+                        break;
+                    case "email":
+                        contactToEdit.setEmail(newValue);
+                        break;
+                    default:
+                        System.out.println("Invalid field to edit!");
+                }
+
+                System.out.println("Contact updated:");
+                contactToEdit.displayContact();
+            } else {
+                System.out.println("Invalid index!");
             }
-
-            System.out.println("Contact updated:");
-            newContact.displayContact();
-        } else if (choice.equalsIgnoreCase("delete")) {
-            System.out.println("Are you sure you want to delete this contact? (yes/no)");
-            String deleteChoice = scanner.nextLine();
-            if (deleteChoice.equalsIgnoreCase("yes")) {
-                newContact = null;
+        } else if (editOrDeleteChoice.equalsIgnoreCase("delete")) {
+            // Delete contact
+            System.out.println("Enter the index of the contact you want to delete:");
+            int index = scanner.nextInt();
+            scanner.nextLine(); // Consume newline character
+            if (index >= 0 && index < addressBookList.size()) {
+                addressBookList.remove(index);
                 System.out.println("Contact deleted.");
+            } else {
+                System.out.println("Invalid index!");
             }
         } else {
             System.out.println("No changes made.");
         }
 
+        // Display all contacts
+        System.out.println("Address Book:");
+        for (int i = 0; i < addressBookList.size(); i++) {
+            System.out.println("Contact " + (i+1) + ":");
+            addressBookList.get(i).displayContact();
+            System.out.println();
+        }
+
         scanner.close();
     }
 }
+
